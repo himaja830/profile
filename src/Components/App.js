8a9dada7490fa3e2db679c4b2fa469f65db8d590
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-09-18 14:03:51 -0500

    changed router to hashrouter

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import LoginCard from "./loginPage";
 import ProfilePage from "./profilePage";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter basename="/">
       <div>
         <Switch>
           <Route exact path="/" component={LoginCard} />
           <Route path="/myprofile" component={ProfilePage} />
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
