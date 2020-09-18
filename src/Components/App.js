04f98e806d2b883512abecfda6fb7aa6c1559886
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-09-18 11:04:56 -0500

    Fix Route issue

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,8 +5,8 @@ import ProfilePage from "./profilePage";
 function App() {
   function getPage() {
     const route = window.location.pathname;
-    if (route === "/Myprofile") return <ProfilePage />;
-    if (route === "/Login") return <LoginCard />;
+    if (route === process.env.PUBLIC_URL + "/Myprofile") return <ProfilePage />;
+    if (route === process.env.PUBLIC_URL + "/Login") return <LoginCard />;
     return <LoginCard />;
   }
   return <div className="container-fluid">{getPage()}</div>;
